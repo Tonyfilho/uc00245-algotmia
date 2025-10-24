d410403ec933cd6a542812063be5df377e97af7c
Tonyfilho <oficinadosdrones@gmail.com>
2025-10-24 18:08:12 +0100

    bug na rota

--- 00245/src/app/app.routes.ts
+++ 00245/src/app/app.routes.ts
@@ -5,7 +5,7 @@ import { Home } from './pages/home/home';
 export const routes: Routes = [
   { path: '', redirectTo: 'aula01', pathMatch: 'full' },
   { path: 'aula01', component: Home },
-  // { path: 'Wellcome', loadComponent: () => import('./pages/wellcome/wellcome').then(c => c.Wellcome)},
+  { path: 'Wellcome', loadComponent: () => import('./pages/wellcome/wellcome').then(c => c.Wellcome)},
   // { path: 'Wellcome', component: Wellcome},
 
   { path: '**', component: Home },
