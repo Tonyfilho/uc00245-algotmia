6fd0596d394c8bab9021839dcd23330a67034635
Tonyfilho <oficinadosdrones@gmail.com>
2025-10-24 15:45:39 +0100

    update

--- 00245/src/app/app.routes.ts
+++ 00245/src/app/app.routes.ts
@@ -2,8 +2,9 @@ import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
 
 export const routes: Routes = [
-//   { path: '', redirectTo: 'home', pathMatch: 'full' },
-//   { path: 'home', component: Home },
+  { path: '', redirectTo: 'wellcome', pathMatch: 'full' },
+  { path: 'home', component: Home },
+  { path: 'Wellcome', loadComponent: () => import('./pages/wellcome/wellcome').then(c => c.Wellcome)},
 
-//   { path: '**', component: Home },
+  { path: '**', component: Home },
 ];
---
+++ 00245/src/app/pages/wellcome/wellcome.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-wellcome',
+  imports: [],
+  templateUrl: './wellcome.html',
+  styleUrl: './wellcome.css'
+})
+export class Wellcome {
+
+}
